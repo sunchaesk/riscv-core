baa1a518f34ae4a1ad877dfc683c38b2418709ac
Chris Kim <sunchaesk@gmail.com>
2024-06-15 10:43:43 -0400

    riscv.v added decoder and tested

--- riscv.v
+++ riscv.v
@@ -1,17 +1,23 @@
 
 module riscv_processor (
-                   input         clk,
-                   input         reset,
-                   output [31:0] pc_out,
-                   output [31:0] instruction_out
-                   );
+                        input         clk,
+                        input         reset,
+                        output [31:0] pc_out,
+                        output [31:0] instruction_out
+                        );
 
    // pc stuff (IFU)
-   reg [31:0]                   pc;
-   reg [31:0]                   fetched_instruction;
+   reg [31:0]                         pc;
+   reg [31:0]                         fetched_instruction;
 
    // decoder initializes (decoder)
-
+   reg [6:0]                          opcode;
+   reg [2:0]                          funct3;
+   reg [6:0]                          funct7;
+   reg [4:0]                          rd;
+   reg [4:0]                          rs1;
+   reg [4:0]                          rs2;
+   reg [31:0]                         imm;
 
    IFU instruction_fetch_unit (
                                .clk(clk),
@@ -22,10 +28,16 @@ module riscv_processor (
                                .instruction(fetched_instruction)
                                );
 
-   // decoder decode_unit (
-   //                      .instruction(fetched_instruction),
-
-   //                      );
+   decoder decode_unit (
+                        .instruction(fetched_instruction),
+                        .opcode(opcode),
+                        .funct3(funct3),
+                        .funct7(funct7),
+                        .rd(rd),
+                        .rs1(rs1),
+                        .rs2(rs2),
+                        .imm(imm)
+                        );
 
 
 
--- test/riscv_tb.v
+++ test/riscv_tb.v
@@ -51,6 +51,24 @@ module riscv_tb;
    wire [31:0] d_pc;
    wire [31:0] d_instruction;
 
+   wire [6:0]                          d_opcode;
+   wire [2:0]                          d_funct3;
+   wire [6:0]                          d_funct7;
+   wire [4:0]                          d_rd;
+   wire [4:0]                          d_rs1;
+   wire [4:0]                          d_rs2;
+   wire [31:0]                         d_imm;
+
+   assign d_pc = core.pc;
+   assign d_instruction = core.fetched_instruction;
+   assign d_opcode = core.opcode;
+   assign d_funct3 = core.funct3;
+   assign d_funct7 = core.funct7;
+   assign d_rd = core.rd;
+   assign d_rs1 = core.rs1;
+   assign d_rs2 = core.rs2;
+   assign d_imm = core.imm;
+
    riscv_processor core (
                          .clk(clk),
                          .reset(reset),
@@ -71,8 +89,8 @@ module riscv_tb;
 
    initial begin
 
-      core.instruction_fetch_unit.instruction_memory[0] = 32'h11111111;
-      core.instruction_fetch_unit.instruction_memory[1] = 32'h22222222;
+      core.instruction_fetch_unit.instruction_memory[0] = 32'h005303b3; // ADD t2, t1, t0
+      core.instruction_fetch_unit.instruction_memory[1] = 32'h00628633; // ADD a2, t0, t1
       core.instruction_fetch_unit.instruction_memory[2] = 32'h33333333;
       core.instruction_fetch_unit.instruction_memory[3] = 32'h44444444;
       core.instruction_fetch_unit.instruction_memory[4] = 32'h55555555;
@@ -84,7 +102,8 @@ module riscv_tb;
       core.instruction_fetch_unit.instruction_memory[10] = 32'hBBBBBBBB;
       core.instruction_fetch_unit.instruction_memory[11] = 32'hCCCCCCCC;
 
-      $monitor("Time = %0t\nPC = 0x%0h\nINSTRUCTION = 0x%0h", $time, pc, instruction);
+      $monitor("Time = %0t\nPC = 0x%0h\nINSTRUCTION = 0x%0h\nOPCODE = 0x%0h\nFUNCT3 = 0x%0h\nFUNCT7 = 0x%0h\nRD = 0x%0h\nRS1 = 0x%0h\nRS2 = 0x%0h\nIMM = 0x%0h\n",
+               $time, d_pc, d_instruction, d_opcode, d_funct3, d_funct7, d_rd, d_rs1, d_rs2, d_imm);
 
       reset = 1;
       #10;
